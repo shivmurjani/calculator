50a665c1744b59a5225593e89d2a92cd43a826de
Shivansh Murjani <murjani.vmware@gmail.com>
2023-08-11 14:02:22 +0530

    finished head structure for calculator, index.html

---
+++ script.js
@@ -0,0 +1,50 @@
+/*  - functions():
+    
+        add,
+    
+        subtract,
+    
+        multiply,
+    
+        devide.
+    
+    - Create 3 variables: 
+        
+        oprand1, 
+        
+        operand2, 
+        
+        operator
+
+    - Create operate():
+        
+        which  takes in 3 variables and one function from above.
+
+    - Create 
+        
+        buttons 0-9, 
+        
+        function(), 
+        
+        equals key, 
+        
+        '.' button to unlock decimal for user,
+        
+        undo button,
+
+        display - filled with temp dummy numbers,
+
+        clear button.
+
+    
+    - Conditionals 
+        
+        calc cannot evaluate more than one operation at a time.
+
+        round answers with long decimals.
+
+        pressing = without inputs will cause problems.
+
+        /0 = pls stop.
+    
+    */
